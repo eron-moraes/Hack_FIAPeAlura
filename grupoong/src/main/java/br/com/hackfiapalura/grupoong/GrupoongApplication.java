ae8935f8b628dbf99b8484a22ee4d1bc4cc09a62
Eron Moraes <eron.moraes7@gmail.com>
2023-10-04 12:12:35 -0300

    project init

---
+++ grupoong/src/main/java/br/com/hackfiapalura/grupoong/GrupoongApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackfiapalura.grupoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoongApplication.class, args);
+	}
+
+}
