b9eb0406ddf93556fdbeb5977a16b16838c18b53
Eron Moraes <eron.moraes7@gmail.com>
2023-10-07 09:28:55 -0300

    Ajuste no DBService para facilitar entendimento

--- grupoong2/src/main/java/br/com/hackfiapalura/grupoong/service/DBService.java
+++ grupoong2/src/main/java/br/com/hackfiapalura/grupoong/service/DBService.java
@@ -19,20 +19,22 @@ public class DBService {
     }
 
     public void inicializarBancoDeDados() {
-        Talent talent1 = new Talent(null, "Roberta", "Tocar piano", 25, "Ficar rica");
-        Talent talent2 = new Talent(null, "Felipe", "Ser dev", 30, "Virar o melhor dev do planeta");
-        Talent talent3 = new Talent(null, "Eron", "ser dev ruim", 28, "terminar esse codigo");
-        Talent talent4 = new Talent(null, "Oliver", "ser ator", 27, "Comprar um hiat");
+        Talent talent1 = new Talent(null, "Roberta", "Negócios", 25, "Ajudar pessoas");
+        Talent talent2 = new Talent(null, "Felipe", "Desenvolvedor", 30, "Programar melhor");
+        Talent talent3 = new Talent(null, "Eron", "Desenvolvedor", 28, "Horas para a faculdade");
+        Talent talent4 = new Talent(null, "Oliver", "Negócios", 27, "Ganhar espaço no mercado de trabalho");
 
         talentRepository.save(talent1);
         talentRepository.save(talent2);
         talentRepository.save(talent3);
         talentRepository.save(talent4);
 
-        Ong ong1 = new Ong(null, "ONG A", "Atividade A", "Financiamento A", "CNPJ A");
-        Ong ong2 = new Ong(null, "ONG B", "Atividade B", "Financiamento B", "CNPJ B");
+        Ong ong1 = new Ong(null, "ONG dos Musicos", "Ensino da Música", "Financiamento A", "12.123.456/0001-10");
+        Ong ong2 = new Ong(null, "ONG dos desenvolvedores", "Ensino de programação", "Financiamento B", "23.456.789/0001-20");
+        Ong ong3 = new Ong(null, "ONG dos navegadores", "Ensino de navegação", "Financiamento C", "23.456.789/0001-30");
 
         ongRepository.save(ong1);
         ongRepository.save(ong2);
+        ongRepository.save(ong3);
     }
 }
